8c0bdfb8efdaba2e329dc20a18a0e1e4093380b8
Kevin Sun <kevingrandflyer@gmail.com>
2024-02-16 18:50:49 -0500

    Config fixes and removed pugixml.

--- RaceSim/src/model/car_factory.cpp
+++ RaceSim/src/model/car_factory.cpp
@@ -5,8 +5,21 @@
 #include <Globals.h>
 #include <v1_car.h>
 
+std::unordered_map<std::string, cars> config_to_car_model = {
+    {"Gen 11.5", cars::V1_CAR},
+};
+std::string DEFAULT_CAR = "Gen 11.5";
+
 Car* Car_Factory::get_car(std::string car_type) {
-    if (car_type == "Gen 11.5") {
+    cars model;
+    if (config_to_car_model.find(car_type) != config_to_car_model.end()) {
+        model = config_to_car_model[car_type];
+    } else {
+        /* Default to gen 11.5 car model */
+        model = config_to_car_model[DEFAULT_CAR];
+    }
+
+    if (model == cars::V1_CAR) {
         spdlog::info("Using V1 Car Model");
         return (Car*) new V1_Car();
     } else {
--- RaceSim/src/optimizer/opt_factory.cpp
+++ RaceSim/src/optimizer/opt_factory.cpp
@@ -4,8 +4,20 @@
 #include <base_opt.h>
 #include <Globals.h>
 
+std::unordered_map<std::string, optimizer> config_to_optimizer = {
+    {"Constant", optimizer::CONSTANT},
+};
+std::string DEFAULT_OPTIMIZER = "Constant";
+
 Optimizer* Opt_Factory::get_optimizer(std::string opt_type, Route route, Sim simulator) {
-    if (opt_type == "Constant") {
+    optimizer opt;
+    if (config_to_optimizer.find(opt_type) != config_to_optimizer.end()) {
+        opt = config_to_optimizer[opt_type];
+    } else {
+        opt = config_to_optimizer[DEFAULT_OPTIMIZER]; /* Default to constant speed */
+    }
+
+    if (opt == optimizer::CONSTANT) {
         spdlog::info("Using constant speed optimizer.");
         return (Optimizer*) new V1_Opt(simulator, route);
     } else {
